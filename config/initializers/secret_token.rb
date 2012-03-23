e62667c5f9f0a1cbce987965c0b2dbf4b38b3440
Stephen Lottermoser <holdtotherod@gmail.com>
2012-03-23 19:36:47 +0000

    Initialize rails app.
    This app should be using rails 3.2 and ruby 1.9.2.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Decide::Application.config.secret_token = 'b098fc4327fbbe7fddee55997210613b7689974e2cf9e6e433c49cf9096a735f310fdb992c8afb3d322cd221d88d4a42accc6df8356385fff9481ccbdd42e818'

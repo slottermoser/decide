cfde3ff068a4bd698eb369eb148bbcf9a392b74b
Stephen Lottermoser <holdtotherod@gmail.com>
2012-03-28 23:20:32 -0600

    Allow for dev enviroment using rails server
    Added a check to see if the root_url indicated the rails
    app was running on a port other than port 80 and hadled it
    properly so the app can be run in a dev environment using the
    built in rails server and a local node server.

--- app/helpers/discussion_helper.rb
+++ app/helpers/discussion_helper.rb
@@ -16,4 +16,14 @@ module DiscussionHelper
     end
     return results
   end
+
+  def event_server_url
+    base = root_url[0...-1]
+    base_parts = base.rpartition(':')
+    # Check to see if we're running on a port other than 80
+    if base_parts[1] == ":"
+      base = base_parts[0]
+    end
+    return base << ":8080"
+  end
 end

44c3a1df48768f7a5f9d4421564bc51bb25c87f0
Stephen Lottermoser <holdtotherod@gmail.com>
2012-04-09 11:01:36 -0600

    Actually fix the bug with root_url
    Oops, didn't correctly fix the bug last time.

--- app/helpers/decisions_helper.rb
+++ app/helpers/decisions_helper.rb
@@ -1,7 +1,7 @@
 module DecisionsHelper
   def event_server_url(rootUrl)
     base = rootUrl[0...-1]
-    base_parts = base.rpartition(':')
+    base_parts = base[6...-1].rpartition(':')
     # Check to see if we're running on a port other than 80
     if base_parts[1] == ":"
       base = base_parts[0]

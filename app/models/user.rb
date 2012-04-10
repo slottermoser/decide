d7aa64c59b51494ef60b813361d541508dab52b6
Devon Humes <devon.humes@gmail.com>
2012-04-10 07:18:26 -0600

    Adding the name to Google OAuth

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,10 +24,11 @@ class User < ActiveRecord::Base
 
   def self.find_for_open_id(access_token, signed_in_resource=nil)
     data = access_token.info
+    puts access_token
     if user = User.where(:email => data["email"]).first
       user
     else
-      User.create!(:email => data["email"], :password => Devise.friendly_token[0,20])
+      User.create!(:email => data["email"], :password => Devise.friendly_token[0,20], :name => data["name"])
     end
   end
 end
